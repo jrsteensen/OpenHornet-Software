8667b8c0bd4aaa1858da7b87b4bc21a6caca1cc1
Arribe <47125808+Arribe@users.noreply.github.com>
2024-03-02 22:50:13 -0600

    refactored_comments-1A7-HUD_PANEL (#74)
    Updated comments per the latest template for docs.

--- embedded/OH1_Upper_Instrument_Panel/1A7-HUD_PANEL/1A7-HUD_PANEL.ino
+++ embedded/OH1_Upper_Instrument_Panel/1A7-HUD_PANEL/1A7-HUD_PANEL.ino
@@ -35,6 +35,7 @@
  * @author Arribe
  * @date 02.29.2024
  * @version 0.0.1
+ * @copyright Copyright 2016-2024 OpenHornet. Licensed under the Apache License, Version 2.0.
  * @brief Controls the HUD panel.
  *
  * @details
@@ -43,7 +44,7 @@
  *  * **Intended Board:** ABSIS ALE
  *  * **RS485 Bus Address:** 3
  * 
- * **Wiring diagram:**
+ * *##Wiring diagram:
  * PIN | Function
  * --- | ---
  * A0   | AOA Indexer
@@ -59,6 +60,13 @@
 *  15   | HUD Symbology NORM
 *  16   | Video Control OFF
 *
+* @brief The following #define tells DCS-BIOS that this is a RS-485 slave device.
+* It also sets the address of this slave device. The slave address should be
+* between 1 and 126 and must be unique among all devices on the same bus.
+*
+* @bug Currently does not work with the Pro Micro (32U4), Fails to compile. 
+*
+*  #define DCSBIOS_RS485_SLAVE 1 ///DCSBios RS485 Bus Address, once bug resolved move line below comment.
 */
 
 /**
@@ -67,9 +75,9 @@
  * 
  */
 #if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega2560__)
-#define DCSBIOS_IRQ_SERIAL
+#define DCSBIOS_IRQ_SERIAL ///< This enables interrupt-driven serial communication for DCS-BIOS. (Only used with the ATmega328P or ATmega2560 microcontrollers.)
 #else
-#define DCSBIOS_DEFAULT_SERIAL
+#define DCSBIOS_DEFAULT_SERIAL ///< This enables the default serial communication for DCS-BIOS. (Used with all other microcontrollers than the ATmega328P or ATmega2560.)  
 #endif
 
 #ifdef __AVR__
@@ -77,47 +85,29 @@
 #endif
 
 /**
- * @brief following #define tells DCS-BIOS that this is a RS-485 slave device.
- * It also sets the address of this slave device. The slave address should be
- * between 1 and 126 and must be unique among all devices on the same bus.
- *
- * @bug Currently does not work with the Pro Micro (32U4), Fails to compile
-*/
-//#define DCSBIOS_RS485_SLAVE 3
-
-/**
  * The Arduino pin that is connected to the
  * RE and DE pins on the RS-485 transceiver.
 */
-#define TXENABLE_PIN 5
-#define UART1_SELECT
+#define TXENABLE_PIN 5 ///< Sets TXENABLE_PIN to Arduino Pin 5
+#define UART1_SELECT ///< Selects UART1 on Arduino for serial communication
 
-/**
- * DCS Bios library include
- */
 #include "DcsBios.h"
 
-/**
- * @brief Define Control I/O for DCS-BIOS. 
- * 
- */
- #define AOA_A A0
- #define BAL_A A1
- #define BLK_A A2
- #define BRT_A A3
- #define REJ_2 6
- #define WB_WB 7
- #define ALT_BARO 8
- #define ATT_STBY 9
- #define ATT_INS 10
- #define DAY_SW 14
- #define REJ_NORM 15
- #define WB_OFF 16
+// Define pins for DCS-BIOS per interconnect diagram.
+ #define AOA_A A0 ///< AOA Indexer
+ #define BAL_A A1 ///< HUD Balance
+ #define BLK_A A2 ///< HUD Black Level
+ #define BRT_A A3 ///< HUD Brightness
+ #define REJ_2 6 ///< HUD Symbology REJ2
+ #define WB_WB 7 ///< Video Control W/B
+ #define ALT_BARO 8 ///< ALT BARO
+ #define ATT_STBY 9 ///< ATT STBY
+ #define ATT_INS 10 ///< ATT INS
+ #define DAY_SW 14 ///< Day Switch
+ #define REJ_NORM 15 ///< HUD Symbology NORM
+ #define WB_OFF 16 ///< Video Control OFF
 
-/**
- * @brief Connect switches to DCS-BIOS 
- * 
- */
+// Connect switches to DCS-BIOS 
 DcsBios::Switch2Pos hudAltSw("HUD_ALT_SW", ALT_BARO);
 DcsBios::Potentiometer hudAoaIndexer("HUD_AOA_INDEXER", AOA_A);
 DcsBios::Switch3Pos hudAttSw("HUD_ATT_SW", ATT_STBY, ATT_INS);
