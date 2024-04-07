d8a205e700d1ab9aed00a595322af1e3df6c617c
Arribe <47125808+Arribe@users.noreply.github.com>
2024-04-06 22:00:16 -0500

    Update ESP_CI_Test.ino

--- embedded/CI_test/ESP_CI_Test/ESP_CI_Test.ino
+++ embedded/CI_test/ESP_CI_Test/ESP_CI_Test.ino
@@ -62,9 +62,10 @@
    #define DCSBIOS_RS485_SLAVE 1 ///DCSBios RS485 Bus Address, once bug resolved move line below comment.
 */
 
+#include <Arduino.h>
 //#include <SPI.h>
 #include <Wire.h>
-#include <Arduino.h>
+
 
 /**
 * Arduino Setup Function
