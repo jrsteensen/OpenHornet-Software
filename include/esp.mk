1ee8da6372d5dfa6f64d8df7c5d1112ea6644648
John Steensen <2449496+jrsteensen@users.noreply.github.com>
2026-05-09 17:33:57 -0600

    fix: correct merge-bin size variable usage in build process

--- include/esp.mk
+++ include/esp.mk
@@ -9,6 +9,7 @@ ESPMK_DIR          = $(ROOTDIR)/include/makeEspArduino
 # found separately by makeEspArduino.
 LIBS               = $(wildcard $(addprefix $(LIBRARY_DIR)/,$(LIBRARIES)))
 CHIP               = esp32
+CPU                ?= esp32
 BUILD_ROOT         = build
 BUILD_DIR          = build
 # Set the compiled firmware filename to the directory name (OpenHornet component name)
@@ -21,6 +22,7 @@ BOOTLOADER_ADDR    = 0x1000
 PARTITIONS_ADDR    = 0x8000
 BOOT_APP_ADDR      = 0xe000
 APP_ADDR           = 0x10000
+MERGE_BIN_SIZE     = $(if $(FLASH_DEF),$(FLASH_DEF),4MB)
 # These flags are set automatically to blank strings.
 # Override them so that the debug build output doesn't fail.
 BUILD_EXTRA_FLAGS  = -DARDUINO_HOST_OS=\"linux\" -DARDUINO_FQBN=\"esp32:esp32:esp32s2\"
@@ -34,7 +36,7 @@ $(MERGED_BIN): all
 	test -f "$(PARTITIONS_BIN)"
 	test -f "$(BOOT_APP_BIN)"
 	test -f "$(MAIN_EXE)"
-	$(ESPTOOL) --chip $(MCU) merge-bin -o $@ --pad-to-size $(FLASH_DEF) --flash-mode keep --flash-freq keep --flash-size keep $(BOOTLOADER_ADDR) "$(BOOTLOADER_BIN)" $(PARTITIONS_ADDR) "$(PARTITIONS_BIN)" $(BOOT_APP_ADDR) "$(BOOT_APP_BIN)" $(APP_ADDR) "$(MAIN_EXE)"
+	$(ESPTOOL) --chip $(MCU) merge-bin -o $@ --pad-to-size $(MERGE_BIN_SIZE) --flash-mode keep --flash-freq keep --flash-size keep $(BOOTLOADER_ADDR) "$(BOOTLOADER_BIN)" $(PARTITIONS_ADDR) "$(PARTITIONS_BIN)" $(BOOT_APP_ADDR) "$(BOOT_APP_BIN)" $(APP_ADDR) "$(MAIN_EXE)"
 
 release: $(MERGED_BIN)
 	cp $(MERGED_BIN) $(RELEASE_DIR)/$(MAIN_NAME).bin
